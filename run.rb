a848cfa047e4d84625a0c8ddeccffdbeb3d69103
Brian <wamwangimathenge@gmail.com>
2023-02-13 09:17:00 +0300

    Array added

---
+++ run.rb
@@ -0,0 +1,75 @@
+require 'pry'
+
+# Enumerables, traversal, searching and sorting
+suv = [
+    {
+        name: "Landcruiser V8",
+        price: 20,000,000,
+        brand: "Toyota",
+        origin: "Japan",
+        engine: "3.3L V6"
+    },
+    {
+        name: "LX-570",
+        price: 25,000,000,
+        brand: "Lexus",
+        origin: "Japan",
+        engine: "3.4L V6"
+    },
+    {
+        name: "Q7",
+        price: 15,000,000,
+        brand: "Audi",
+        origin: "Germany",
+        engine: "3.0L V6"
+    },
+    {
+        name: "Bentayga",
+        price: 35,000,000,
+        brand: "Bentley",
+        origin: "UK",
+        engine: "4.0L V8"
+    },
+    {
+        name: "Suburban",
+        price: 35,000,000,
+        brand: "Chevrolet",
+        origin: "USA",
+        engine: "3.3L"
+    },
+    {
+        name: "Escalade",
+        price: 45,000,000,
+        brand: "Cadillac",
+        origin: "USA",
+        engine: "6.2L V8"
+    },
+    {
+        name: "Range Rover Vogue",
+        price: 25,000,000,
+        brand: "Land-Rover",
+        origin: "UK",
+        engine: "4.4L V8"
+    },
+    {
+        name: "X7",
+        price: 25,000,000,
+        brand: "BMW",
+        origin: "Germany",
+        engine: "4.4L V8"
+    },
+    {
+        name: "Urus",
+        price: 45,000,000,
+        brand: "Lamboghini",
+        origin: "Italy",
+        engine: "4.0L V8"
+    },
+    {
+        name: "G-Wagon",
+        price: 40,000,000,
+        brand: "Mercedes",
+        origin: "Germany",
+        engine: "4.0L V8"
+    },
+]
