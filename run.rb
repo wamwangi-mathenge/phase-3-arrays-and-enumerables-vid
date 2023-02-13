c5a35010507b24c99734c7a3387c3d1698a2ba09
Brian <wamwangimathenge@gmail.com>
2023-02-13 09:33:29 +0300

    Each loop

--- run.rb
+++ run.rb
@@ -1,75 +1,86 @@
 require 'pry'
 
 # Enumerables, traversal, searching and sorting
-suv = [
+suvs = [
     {
         name: "Landcruiser V8",
-        price: 20,000,000,
+        price: 20000000,
         brand: "Toyota",
         origin: "Japan",
         engine: "3.3L V6"
     },
     {
         name: "LX-570",
-        price: 25,000,000,
+        price: 25000000,
         brand: "Lexus",
         origin: "Japan",
         engine: "3.4L V6"
     },
     {
         name: "Q7",
-        price: 15,000,000,
+        price: 15000000,
         brand: "Audi",
         origin: "Germany",
         engine: "3.0L V6"
     },
     {
         name: "Bentayga",
-        price: 35,000,000,
+        price: 35000000,
         brand: "Bentley",
         origin: "UK",
         engine: "4.0L V8"
     },
     {
         name: "Suburban",
-        price: 35,000,000,
+        price: 35000000,
         brand: "Chevrolet",
         origin: "USA",
         engine: "3.3L"
     },
     {
         name: "Escalade",
-        price: 45,000,000,
+        price: 45000000,
         brand: "Cadillac",
         origin: "USA",
         engine: "6.2L V8"
     },
     {
         name: "Range Rover Vogue",
-        price: 25,000,000,
+        price: 25000000,
         brand: "Land-Rover",
         origin: "UK",
         engine: "4.4L V8"
     },
     {
         name: "X7",
-        price: 25,000,000,
+        price: 25000000,
         brand: "BMW",
         origin: "Germany",
         engine: "4.4L V8"
     },
     {
         name: "Urus",
-        price: 45,000,000,
+        price: 45000000,
         brand: "Lamboghini",
         origin: "Italy",
         engine: "4.0L V8"
     },
     {
         name: "G-Wagon",
-        price: 40,000,000,
+        price: 40000000,
         brand: "Mercedes",
         origin: "Germany",
         engine: "4.0L V8"
     },
 ]
+
+# each
+# for every element in the array, each will go over each one
+# loops through arrays
+
+def print_suvs
+    suvs.each do |s|
+        binding.pry
+        puts s
+    end
+end
